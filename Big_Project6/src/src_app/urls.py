51446699687c03e927a23bb8cb9473c50c9d5411
Nuran <berdaliyev2005@mail.ru>
2026-05-27 00:30:23 +0500

    Replace function-based views to class-based views

--- Big_Project6/src/src_app/urls.py
+++ Big_Project6/src/src_app/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from . import views
+from .views import (TaskListView, 
+                    TaskDetailView, 
+                    ProjectListView, 
+                    ProjectDetailView,
+                    CategoryListView,
+                    CategoryDetailView)
+
 
 urlpatterns = [
-    path('tasks/', views.tasks_list, name='tasks_list'),
-    path('tasks/<int:task_id>/', views.tasks_detail, name='tasks_detail'),
-    path('projects/', views.projects_list, name='projects_list'),
-    path('projects/<int:project_id>/', views.projects_detail, name='projects_detail'),
-    path('categories/', views.categories_list, name='categories_list'),
-    path('categories/<int:category_id>/', views.categories_detail, name='categories_detail'),
+    path('tasks/', TaskListView.as_view(), name='tasks_list'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='tasks_detail'),
+    path('projects/', ProjectListView.as_view(), name='projects_list'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='projects_detail'),
+    path('categories/', CategoryListView.as_view(), name='categories_list'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='categories_detail'),
 ]
--- Big_Project6/src/src_app/views.py
+++ Big_Project6/src/src_app/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Task, Category, Project
+from unicodedata import category
 
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Task, Category, Project
+'''
 def tasks_list(request):
     tasks=Task.objects.all()
     return render(request, 'tasks/tasks_list.html', {"tasks": tasks})
 
+'''
+class TaskListView(ListView):
+    model=Task
+    template_name='tasks/tasks_list.html'
+    context_object_name='tasks'
+'''
 def tasks_detail(request, task_id):
     task=get_object_or_404(Task, id=task_id)
     return render(request, 'tasks/tasks_detail.html', {"task": task})
+'''
+class TaskDetailView(DetailView):
+    model=Task
+    template_name='tasks/tasks_detail.html'
+    context_object_name='task'
 
+'''
 def projects_list(request):
     projects=Project.objects.all()
     return render(request, 'projects/projects_list.html', {"projects": projects})
+'''
 
+class ProjectListView(ListView):
+    model=Project
+    template_name='projects/projects_list.html'
+    context_object_name='projects'
+
+'''
 def projects_detail(request, project_id):
     project=get_object_or_404(Project, id=project_id)
     tasks=Task.objects.filter(project=project)
@@ -20,11 +42,26 @@ def projects_detail(request, project_id):
         "project": project,
         "tasks": tasks
     })
-
+'''
+class ProjectDetailView(DetailView):
+    model=Project
+    template_name='projects/projects_detail.html'
+    context_object_name='project'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['tasks']=Task.objects.filter(project=self.object)
+        return context
+'''
 def categories_list(request):
     categories=Category.objects.all()
     return render(request, 'categories/categories_list.html', {"categories": categories})
+'''
+class CategoryListView(ListView):
+    model=Category
+    template_name='categories/categories_list.html'
+    context_object_name='categories'
 
+'''
 def categories_detail(request, category_id):
     category=get_object_or_404(Category, id=category_id)
     tasks=Task.objects.filter(category=category)
@@ -32,3 +69,15 @@ def categories_detail(request, category_id):
         "category": category,
         "tasks": tasks
     })
+'''
+
+class CategoryDetailView(DetailView):
+    model=Category
+    template_name='categories/categories_detail.html'
+    context_object_name='category'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks']=Task.objects.filter(category=self.object)
+        return context
+    
+    
